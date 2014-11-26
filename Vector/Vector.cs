9a516cc9c84649e0a67eeb79a638f4673f8a7602
GadjievAlex <gadjievalex@inbox.ru>
2014-11-26 16:29:52 +0300

    Add
    Desc

--- Vector/Vector.cs
+++ Vector/Vector.cs
@@ -12,8 +12,48 @@ namespace DefaultNamespace
 	/// </summary>
 	public class Vector
 	{
-		public Vector()
-		{
-		}
+		private double fx;
+        private double fy;
+ 
+        public Vector(double ax, double ay)
+        {
+            fx = ax;
+            fy = ay;
+        }
+ 
+        public double x
+        {
+            get
+            {
+                return fx;
+            }
+            set
+            {
+                fx = value;
+            }
+        }
+ 
+        public double y
+        {
+            get
+            {
+                return fy;
+            }
+            set
+            {
+                fy = value;
+            }
+ 
+        }
+ 
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x + v2.x, v1.y + v2.y);
+        }
+ 
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x - v2.x, v1.y - v2.y);
+        }
 	}
 }
