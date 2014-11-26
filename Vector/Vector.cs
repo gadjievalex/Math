07d781a3e07ae9b9e71303844b504ea48bfbfd4c
GadjievAlex <gadjievalex@inbox.ru>
2014-11-26 16:14:50 +0300

    Added Vector
    Vector

---
+++ Vector/Vector.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: GadjievAlex
+ * Дата: 26.11.2014
+ */
+using System;
+
+namespace DefaultNamespace
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class Vector
+	{
+		public Vector()
+		{
+		}
+	}
+}
